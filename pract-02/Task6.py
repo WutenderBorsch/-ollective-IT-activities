f2c7f21b14b8f406614fe51baeeb0f4e9b683abb
Faulheit <73297011+WutenderBorsch@users.noreply.github.com>
2021-04-20 17:41:17 +0300

    Update Task6.py
    Исправлено: задание было реализовано не как функция

--- pract-02/Task6.py
+++ pract-02/Task6.py
@@ -1,13 +1,18 @@
 import numpy as np;
 
-print('Введите размер квадратной матрицы')
+def SumOfElements(x):
+    if a == b:
+        z=[m[i][i] for i in range(len(m))]
+        return sum(z)
+    else:return -1
+    
+print('Введите размер квадратной матрицы:')
 a=int(input())
 b=int(input())
 m = np.random.randint(0, 10, (a, b))
-if a == b:
-    z=[m[i][i] for i in range(len(m))]
-    print(m)
-    print(sum(z))
-else: 
+print(m)
+k = SumOfElements(m)
+if(k == -1):
     print('Вы ввели размер не квадратной матрицы!')
-
+else:
+    print(k)
