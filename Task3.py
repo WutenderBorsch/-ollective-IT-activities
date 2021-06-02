e9d6f5611b510558005dfa153383750a8f3394c7
Veronika2125 <62997381+Veronika2125@users.noreply.github.com>
2021-06-02 18:31:19 +0300

    Update Task3.py

--- Task3.py
+++ Task3.py
@@ -1,8 +1,12 @@
 import numpy as np
 
+def СyclicShift(a):
+    b = a[k:]+a[:k]
+    return b
+
 a = [1, 2, 3, 4, 5]  
 print (a) # вывод Массива
 print('Введите  число, на которое сместить массив: \t')
 k = int(input())
-b = a[k:]+a[:k]
-print(b)
+print(СyclicShift(a))
+print('')
