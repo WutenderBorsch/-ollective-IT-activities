a4044bee65c861e7dabb0bdaeadaa70597f63662
Veronika2125 <62997381+Veronika2125@users.noreply.github.com>
2021-06-02 18:26:15 +0300

    Практика №2. Алгоритм классификации k ближайших соседей

---
+++ kNN.py
@@ -0,0 +1,34 @@
+import numpy as np
+import math
+
+def k_nearest(X, k, obj):
+    
+    # нормализация каждого столбца (кроме последнего)
+    sub_X = X[:, 0:-1]
+    mx=np.max(sub_X, axis=0)
+    norm_X = sub_X/mx
+    
+    # нормализация объекта obj
+    norm_obj = obj/mx
+    
+    distance = np.zeros((16, k-1))
+    for i in range(16):
+       distance[i] = dist(norm_X[i], norm_obj)
+   
+    # получение с помощью функции np.argsort индексы соседей по мере их удаления от obj.
+    sort = np.argsort(distance, axis=0)
+    
+    # выборка в отдельный вектор классы k ближайших соседей
+    nearest_classes = np.zeros((4))
+    for i in range(4):
+        index = sort[i,0]
+        nearest_classes[i] = X[[index], -1]   
+
+    # наиболее часто встречающийся класс в этом векторе
+    unique, counts = np.unique(nearest_classes, return_counts=True)
+    object_class = unique[np.argmax(counts)]
+    return object_class
+
+# вычисление евклидова расстояния между двумя точками
+def dist(p1, p2):
+    return math.sqrt(sum((p1-p2)**2))
