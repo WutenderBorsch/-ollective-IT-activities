d77604d7a205e75ba8ca609006d5662e510a29dd
Veronika2125 <62997381+Veronika2125@users.noreply.github.com>
2021-06-03 21:21:52 +0300

    Update k_means.py

--- pract-03/k_means.py
+++ pract-03/k_means.py
@@ -1,35 +1,30 @@
-﻿import math
+import math
 import numpy as np
 
 # евклидово расстояние между двумя точками
-def dist(A, B):
-  r=math.sqrt(sum((A-B)**2))
-  return r
+def euclidean_distance(A, B):
+  if (isinstance(A, np.ndarray) == 1 & isinstance(B, np.ndarray) == 1):
+    return math.sqrt(sum((A-B)**2))
 
 # возвращает список индексов ближайших центров по каждой точке
 def class_of_each_point(X, centers):
   m = len(X)
   k = len(centers)
-   # матрица расстояний от каждой точки до каждого центра
-  distances = np.zeros((m, k))
-  for i in range(m):
-    for j in range(k):
-        distances[i, j] = dist(centers[j], X[i])
+   
+  # матрица расстояний от каждой точки до каждого центра
+  distances = [[euclidean_distance(centers[j], X[i]) for j in range(k)] for i in range(m)]
+        
   # поиск ближайшего центра для каждой точки
   return np.argmin(distances, axis=1)
 
 def kmeans(k, X):
-  # TODO: инициализировать переменные m и n
-  m = X.shape[0]
-  n = X.shape[1]
+  m, n = X.shape
   curr_iteration = prev_iteration = np.zeros(m) 
-  # TODO: сгенерировать k кластерных центров со случайными координатами.
   mn=np.min(X, axis=0) 
   mx=np.max(X, axis=0) 
   centers = np.random.uniform(mn, mx, (k, n))
   # приписываем каждую точку к заданному классу
   curr_iteration = class_of_each_point(X, centers)
-  # TODO: условие выхода из цикла - векторы curr_iteration и prev_iteration стали равны
   while np.any(curr_iteration != prev_iteration):
     prev_iteration = curr_iteration
     # вычисляем новые центры масс
