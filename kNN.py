8b46fb6cdd96fd7a1ca253657260102f9f47f2e5
Veronika2125 <62997381+Veronika2125@users.noreply.github.com>
2021-06-04 14:39:45 +0300

    Update kNN.py

--- kNN.py
+++ kNN.py
@@ -3,23 +3,20 @@ import math
 
 def k_nearest(X, k, obj):
     
-    # нормализация каждого столбца (кроме последнего)
-    mx=np.max(X[:, 0:-1], axis=0)
-    norm_X = X[:, 0:-1]/mx
+    # стандартизаия каждого столбца (кроме последнего)
     
-    # нормализация объекта obj
-    norm_obj = obj/mx
-    distance = np.zeros((len(X), k-1))
-    for i in range(len(X)):
-       distance[i] = dist(norm_X[i], norm_obj)
+    mean = X[:, 0:-1].mean(axis = 0)
+    std = X[:, 0:-1].std(axis = 0)
+    norm_X = X[:, 0:-1] - mean/std
+    
+    # стандартизация объекта obj
+    norm_obj = obj - mean / std
+    distance = [[dist(norm_X[i], norm_obj)] for i in range(len(X))]
     # получение с помощью функции np.argsort индексы соседей по мере их удаления от obj.
     sort = np.argsort(distance, axis=0)
     
     # выборка в отдельный вектор классы k ближайших соседей
-    nearest_classes = np.zeros((4))
-    for i in range(4):
-        index = sort[i,0]
-        nearest_classes[i] = X[[index], -1]   
+    nearest_classes = [X[sort[i,0], -1] for i in range(k)]
 
     # наиболее часто встречающийся класс в этом векторе
     unique, counts = np.unique(nearest_classes, return_counts=True)
@@ -29,4 +26,5 @@ def k_nearest(X, k, obj):
 # вычисление евклидова расстояния между двумя точками
 def dist(p1, p2):
     return math.sqrt(sum((p1-p2)**2))
+
 
