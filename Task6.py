c5d178a729051c81ce8d85dbdaa308d47870b26a
Veronika2125 <62997381+Veronika2125@users.noreply.github.com>
2021-06-07 19:41:59 +0300

    Update Task6.py

--- Task6.py
+++ Task6.py
@@ -1,17 +1,18 @@
 import numpy as np;
 
 def SumOfElements(x):
-    if a == b:
-        z=[m[i][i] for i in range(len(m))]
+    n, m = x.shape
+    if n == m:
+        z=[x[i][i] for i in range(n)]
         return sum(z)
     else: return -1
     
 print('Введите размер квадратной матрицы')
-a=int(input())
-b=int(input())
-m = np.random.randint(0, 10, (a, b))
-print(m)
-k = SumOfElements(a)
+n=int(input())
+m=int(input())
+arr = np.random.randint(0, 10, (n, m))
+print(arr)
+k = SumOfElements(arr)
 if(k == -1): 
     print('Вы ввели размер не квадратной матрицы!')
 else: 
